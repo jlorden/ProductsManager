cc4b9f7102156fdffe49385287bc8e5bf5d31afb
Jlorden <Jamie.lorden@gmail.com>
2020-05-21 13:20:16 -0500

    second commit

--- server/controllers/products.controller.js
+++ server/controllers/products.controller.js
@@ -9,15 +9,19 @@ module.exports.returnAllProducts = (req, res) => {
 module.exports.createProduct = (req, res) => {
     console.log(req.body);
     Product.create(req.body)
-        .then(newProduct => {
-            res.json({ product: newProduct })
-            console.log("in the controller");
-        })
+        .then(newProduct => res.json({ product: newProduct }))
         .catch(err => res.json( err ));
-};
+    };
 
-module.exports.returnOneProduct = (req, res) => {
-	Product.findOne({ _id: req.params.id })
-		.then(oneProduct => res.json({ product: oneProduct }))
-		.catch(err => res.json(err));
-};
+
+    module.exports.updateProduct = (req, res) => {
+    Product.findByIdAndUpdate({ _id: req.params._id }, req.body, {runValidators:true})
+        .then(editProduct => res.json({ product: editProduct }))
+        .catch(err => res.json({err }));
+    };
+
+    module.exports.deleteProduct = (req, res) => {
+    Product.findByIdAndDelete({ _id: req.params._id })
+        .then(() => res.json({ msg: "tisdone"} ))
+        .catch(err => res.json(  err ));
+    };
--- server/routes/products.routes.js
+++ server/routes/products.routes.js
@@ -1,7 +1,8 @@
 const productController = require("../controllers/products.controller");
 
 module.exports = app => {
-    app.get("/api/products/", productController.returnAllProducts);
-    app.get("/api/products/:id", productController.returnOneProduct );
-    app.post("/api/products/", productController.createProduct);
+  app.get("/api/products/", productController.returnAllProducts);
+  app.post("/api/products/", productController.createProduct);
+  app.put("/api/products/:_id", productController.updateProduct);
+  app.delete("/api/products/:_id", productController.deleteProduct);
 };
