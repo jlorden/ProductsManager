f6675cb43f49a37872363e487da89f7aa9ba801f
Jlorden <Jamie.lorden@gmail.com>
2020-05-22 10:08:16 -0500

    Final commit

--- server/controllers/products.controller.js
+++ server/controllers/products.controller.js
@@ -1,27 +1,33 @@
 const Product = require("../models/products.model");
 
 module.exports.returnAllProducts = (req, res) => {
-    Product.find()
-        .then(allProducts => res.json({ product: allProducts }))
-        .catch(err => res.json({ message: "warning... ", error: err }));
-    };
+Product.find()
+    .then(allProducts => res.json({ product: allProducts }))
+    .catch(err => res.json({ message: "warning... ", error: err }));
+};
 
 module.exports.createProduct = (req, res) => {
-    console.log(req.body);
-    Product.create(req.body)
-        .then(newProduct => res.json({ product: newProduct }))
-        .catch(err => res.json( err ));
-    };
+console.log(req.body);
+Product.create(req.body)
+    .then(newProduct => res.json({ product: newProduct }))
+    .catch(err => res.json( err ));
+};
 
+module.exports.returnOneProduct = (req, res) => {
+    Product.findOne({ _id: req.params._id })
+        .then(oneProduct => res.json({ product: oneProduct }))
+        .catch(err => res.json(err));
+};
 
-    module.exports.updateProduct = (req, res) => {
-    Product.findByIdAndUpdate({ _id: req.params._id }, req.body, {runValidators:true})
-        .then(editProduct => res.json({ product: editProduct }))
-        .catch(err => res.json({err }));
-    };
+module.exports.updateProduct = (req, res) => {
+Product.findByIdAndUpdate({ _id: req.params._id }, req.body, {runValidators:true})
+    .then(editProduct => res.json({ product: editProduct }))
+    .catch(err => res.json(err));
+};
 
-    module.exports.deleteProduct = (req, res) => {
-    Product.findByIdAndDelete({ _id: req.params._id })
-        .then(() => res.json({ msg: "tisdone"} ))
-        .catch(err => res.json(  err ));
-    };
+module.exports.deleteProduct = (req, res) => {
+// console.log("in the controller", req.params._id )
+Product.findByIdAndDelete({ _id: req.params._id })
+    .then(() => res.json({ msg: "tisdone"} ))
+    .catch(err => res.json(  err ));
+};
